93594571163207bf54b3ae27d9344add696bda7d
dkhachatrian <david.g.khachatrian@gmail.com>
2015-07-28 16:12:57 -0700

    Fixed Unicode problem!
    Been using this as a test area for getting people as well...
    Using the Kitchen module, can properly read the strings with UTF-8 encoding.
    
    TODO: - work in helper_functions to get names

--- NLTK_Proper_Noun_Extractor.py
+++ NLTK_Proper_Noun_Extractor.py
@@ -1,33 +1,42 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import nltk
-#import codecs  #Python 2
 
+#os.path.join(path, path, ...)
 #### To ensure the working directory starts at where the script is located...
 import os
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
 ####
-
+#import nltk
+import codecs  #Python 2
+from lib import helper_functions as hf
+import kitchen.text.converters as k
 
 #with codecs.open("Scaloria Figure Captions.txt", 'r', encoding = 'utf-8') as f:    #Python 2
-with open("Cleaned Captions.txt", 'r', encoding = 'utf-8') as f:
-        with open("Capitalized_Word_List.txt" , 'w', encoding = 'utf-8') as w:
+with codecs.open("Cleaned Captions.txt", 'r', encoding = 'utf-8') as f:
+        with codecs.open("Capitalized_Word_List.txt" , 'w', encoding = 'utf-8') as w:
                 lines = f.readlines()
 
                 for line in lines:
-                    s = line
-#                    s = line.decode('utf8')    #Python 2 (?)
-                    tokens = nltk.word_tokenize(s) #break line into word "tokens"
-                    tagged = nltk.pos_tag(tokens) #assign tags to token's role in sentence
-                    entities = nltk.ne_chunk(tagged)
-                    proper_nouns = [n[0] for n in tagged if 'NNP' in n[1]] #list of nouns in line (second entry in tuple gives tag)
+                    #print(line)
+                    u = k.to_unicode(line, encoding = 'utf-8')
+                    s = hf.Line(u)
+                    proper_nouns = s.get_proper_nouns()
+                    people = s.get_people()
 
                     if len(proper_nouns) > 0:
-                                w.write("List of capitalized phrases found in " + s.split()[0] + " are the following: " + '\n')
+                                w.write("List of capitalized phrases found in " + line.split()[0] + " are the following: " + '\n')
                                 for noun in proper_nouns:
-                                        w.write(noun + '\t')
-                                for x in range(2):
-                                        w.write('\n') #twice to make it easier on the eyes to separate captions
+                                    w.write(noun + '\t')
+                                w.write('\n' + "And the people found in this caption are the following: " + '\n')
+                                for person in people:
+                                    w.write(person + '\t')
+                                ##debugging...
+                                #w.write("Corresponding tuples:" + '\n')
+                                #for tup in all_tuples:
+                                #    w.write(str(tup) + '\t')
+                                #    
+                                for x in range(3):
+                                    w.write('\n') #twice to make it easier on the eyes to separate captions
 
