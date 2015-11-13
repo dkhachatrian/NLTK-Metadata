949993f69ad9deb0ce1ee6673dd80e01cfee5e06
dkhachatrian <david.g.khachatrian@gmail.com>
2015-11-12 17:04:55 -0800

    Update shared.py
    Combined shared variables there.

--- lib/shared.py
+++ lib/shared.py
@@ -7,6 +7,45 @@ not_found = ""
 creator_leading_phrases = ['by', 'courtesy of']
 wnl = nltk.WordNetLemmatizer()
 
+
+###############################################
+######### A bunch of declarations...###########
+###############################################
+
+#Worth noting: when writing to file, will need to convert strings to bytes using bytes(string, encoding scheme)
+
+DELIMITER = '\t'  #will be tab-separated
+IN_CELL_DELIMITER = ',' #when phrases need to be differentiated within a cell in the CSV
+PERSONS_MAX = 10 #total number of people of a specific type, e.g. Creator or Author, in a row. Determined by Metadata Fields.txt.
+                #To simplify code, all the different types of people have the same max.
+
+excelHeader = ""
+metadataFields = [] #list
+
+with open("./dependencies/Metadata Fields.txt", 'r') as mf:
+        #may want to change the wording of the below lines, would mess up if there were more than one line
+        for line in mf: #should only be the one line with column headers
+                metadataFields = line.split('\t') #splits into list using comma as delimiter (for .csv)
+        for entry in metadataFields:
+                excelHeader = excelHeader + entry + DELIMITER
+#        for line in mf:
+#                metadataFields.append(line[:-1])    #line[:-1] removes '\n' from line
+#                excelHeader = excelHeader + line[:-1] + DELIMITER
+        #excelHeader has extra \t at end
+        excelHeader = excelHeader[0:-1] #cut off last \t
+        #excelHeader does NOT have a \n at the end
+
+
+
+
+
+
+
+
+
+
+
+
 #
 ###### COPYPASTA #####
 #
