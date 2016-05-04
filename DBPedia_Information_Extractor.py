8c3cc8cb22d2c19e82ad77707ba59497d4c38dd3
David Khachatrian <david.g.khachatrian@gmail.com>
2016-05-04 15:42:54 -0700

    Prepping for Spotlight.
    (Preliminary results not ideal, but will prepare merging functions regardless.) Merging dicts and writing them to file.

--- DBPedia_Information_Extractor.py
+++ DBPedia_Information_Extractor.py
@@ -10,10 +10,14 @@ import os
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
+root = os.path.dirname(dname)
 ####
 
 import urllib.request #lets us request information (GET) from websites
 import urllib.parse #helps encode POST request parameters
+
+import time #can do time.sleep(numberOfSeconds)
+import spotlight #wrapper for DBPedia Spotlight
 
 # example: urllib.parse.urlencode(dictionaryOfVariableNamesToValues)
 # data = data.encode('uft-8') //converts data (e.g. dictionary) to bytes
@@ -21,23 +25,42 @@ import urllib.parse #helps encode POST request parameters
 
 # open our file with all the caption lines separated by newlines
 
-PRECONDITION: have a sanitized caption file
+
+with open(os.path.join(root, 'dependencies', 'test_captions.txt'), 'r', encoding='utf-8') as inf, open(os.path.join(root, 'dependencies', 'test_map.txt'), 'w', encoding='utf-8') as outf:
+    for line in inf:
+        cl = line.strip("\n")
+        annotated = spotlight.annotate('http://spotlight.sztaki.hu:2222/rest/annotate', cl, confidence=0.4, support=20, spotter='Default')
+        
+        
+        
+        time.sleep(1) #wait a second between each call
+                
+        
+        
+#for line in lines_from_file:
 
 
 
 
-for each line in the file:
-  request an annotation of the line with confidence=0.35(?) to URL http://spotlight.sztaki.hu:2222/rest/annotate
-  save into a hashmap file lineNumber->annotatedText i(use linked list?) //so we don't have to call DBPedia service constantly...
-  
-  for each annotated phrase:
-      follow associated link //surrounded by <a href="http://www.example.com">example text</a>
-      get page source
-      search for and grab the information of interest // classification (the word(s) that comes after "An Entity of Type : ") 
-      save into a hashmap (word->DBPType) //we will manually create a common-sense mapping of DBPedia type -> our types
-      
 
 
-// later, when actually making spreadsheet,
-// will search for values in the hashmaps (line->annotatedText->dbpediaType->ourType) for each entity
-// (This will have trouble coupling names with roles...)
+#PRECONDITION: have a sanitized caption file
+#
+#
+#
+#
+#for each line in the file:
+#  request an annotation of the line with confidence=0.35(?) to URL http://spotlight.sztaki.hu:2222/rest/annotate
+#  save into a hashmap file lineNumber->annotatedText i(use linked list?) //so we don't have to call DBPedia service constantly...
+#  
+#  for each annotated phrase:
+#      follow associated link //surrounded by <a href="http://www.example.com">example text</a>
+#      get page source
+#      search for and grab the information of interest // classification (the word(s) that comes after "An Entity of Type : ") 
+#      save into a hashmap (word->DBPType) //we will manually create a common-sense mapping of DBPedia type -> our types
+#      
+#
+#
+#// later, when actually making spreadsheet,
+#// will search for values in the hashmaps (line->annotatedText->dbpediaType->ourType) for each entity
+#// (This will have trouble coupling names with roles...)
